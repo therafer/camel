8c9dfd4c3a0ac0185ff517ec641d36b6bc294f76
camel <camel@therafer.(none)>
2012-10-04 17:55:35 -0400

    Add rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Camel::Application.config.secret_token = 'ae169cf54fe9b62288adcf478001d25cbaf3c056ace1aca9d6817932b07ae8946383aec4a54d8cea80fb4f982b025ebbcf64a15c2c1134b4969e47ce052713b6'
